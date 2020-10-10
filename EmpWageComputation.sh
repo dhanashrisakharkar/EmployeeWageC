eff21cc7c8cd8cf0278ed9bb0a669c74ceeb88cf
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-10 19:36:07 +0530

    Added : Employee Attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,3 +2,12 @@
 
 
 echo "welcome to Employee Wage Computation"
+
+attendance=$((RANDOM%2))
+
+if [ $attendance -eq 1 ]
+then 
+     echo " employee is present "
+else 
+     echo " employee is absent "
+fi
