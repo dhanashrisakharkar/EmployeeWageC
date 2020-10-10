b030b1c6e0881499fe11818af07a1ec2e4427889
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-10 22:33:55 +0530

    Added : Using Switch Case Statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -9,24 +9,26 @@ PART_TIME_HOUR_IS=8
 
 attendance=$((RANDOM%3))
 
-if [ $attendance -eq 1 ]
-then 
-     
-     echo " employee is Present "
-     SALARY=$(($WAGE_PER_HOUR_IS*$FULL_DAY_HOUR_IS))
-     echo "Daily Employee Wage Is" : $SALARY
+case $attendance in
+                  1  )
 
-elif [ $attendance -eq 2 ]
-then
-     echo " employee is present for half day "
-     SALARY=$(($WAGE_PER_HOUR_IS*$PART_TIME_HOUR_IS))
-     echo "Part Time Hour Is" : $PART_TIME_HOUR_IS
-     echo "Part Time Employee Wage Is" : $SALARY
-     
-else 
-     
-     echo " employee is Absent "
-     SALARY=0
-     echo "Employee Wage Is" : $SALARY
-fi
- 
+                     echo " employee is Present "
+                     SALARY=$(($WAGE_PER_HOUR_IS*$FULL_DAY_HOUR_IS))
+                     echo "Daily Employee Wage Is" : $SALARY
+                     ;;
+
+                  2  )  
+
+                     echo " employee is present for half day "
+                     SALARY=$(($WAGE_PER_HOUR_IS*$PART_TIME_HOUR_IS))
+                     echo "Part Time Hour Is" : $PART_TIME_HOUR_IS
+                     echo "Part Time Employee Wage Is" : $SALARY
+                     ;;
+
+                 *  )
+
+                    echo " employee is Absent "
+                    SALARY=0
+                    echo "Employee Wage Is" : $SALARY
+                    ;;
+esac
