f9d149d4cd7d44aa1b0f21a1c47f2dd6a8cc3fec
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-10 23:03:38 +0530

    Added : Wages For A Month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -6,8 +6,15 @@ echo "welcome to Employee Wage Computation"
 WAGE_PER_HOUR_IS=20
 FULL_DAY_HOUR_IS=16
 PART_TIME_HOUR_IS=8
+WORKING_DAY_PER_MONTH=20
+
+Total_Salary=0
+
+for ((a=1; a<$WORKING_DAY_PER_MONTH; a++))
+do
 
 attendance=$((RANDOM%3))
+
 
 case $attendance in
                   1  )
@@ -31,4 +38,21 @@ case $attendance in
                     SALARY=0
                     echo "Employee Wage Is" : $SALARY
                     ;;
+
 esac
+
+                   if [ $SALARY -eq 320 ]
+                   then
+
+                      Total_Salary=$(($Total_Salary+$SALARY))
+                      echo "Full Time Employee Total Salary Is" : $Total_Salary
+
+                   else
+
+                      Total_Salary=$(($Total_Salary+$SALARY))
+                      echo "Part Time Employee Total Salary Is" : $Total_Salary
+
+                   fi
+
+
+done
