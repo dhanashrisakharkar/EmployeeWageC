634603f885f72c9a9539a02fad0bdbd45fe3efa5
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-10 22:20:33 +0530

    Added : Part Time Employee And Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,18 +4,25 @@
 echo "welcome to Employee Wage Computation"
 
 WAGE_PER_HOUR_IS=20
-FULL_DAY_HOUR_IS=8
+FULL_DAY_HOUR_IS=16
+PART_TIME_HOUR_IS=8
 
-
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 
 if [ $attendance -eq 1 ]
 then 
      
      echo " employee is Present "
      SALARY=$(($WAGE_PER_HOUR_IS*$FULL_DAY_HOUR_IS))
-    echo "Daily Employee Wage Is" : $SALARY
+     echo "Daily Employee Wage Is" : $SALARY
 
+elif [ $attendance -eq 2 ]
+then
+     echo " employee is present for half day "
+     SALARY=$(($WAGE_PER_HOUR_IS*$PART_TIME_HOUR_IS))
+     echo "Part Time Hour Is" : $PART_TIME_HOUR_IS
+     echo "Part Time Employee Wage Is" : $SALARY
+     
 else 
      
      echo " employee is Absent "
