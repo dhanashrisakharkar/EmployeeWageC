87cc09a0784ba60bb80ea57cc8eb74b1ab7fc592
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-10 22:02:44 +0530

    Added : Daily Employee Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -3,11 +3,23 @@
 
 echo "welcome to Employee Wage Computation"
 
+WAGE_PER_HOUR_IS=20
+FULL_DAY_HOUR_IS=8
+
+
 attendance=$((RANDOM%2))
 
 if [ $attendance -eq 1 ]
 then 
-     echo " employee is present "
+     
+     echo " employee is Present "
+     SALARY=$(($WAGE_PER_HOUR_IS*$FULL_DAY_HOUR_IS))
+    echo "Daily Employee Wage Is" : $SALARY
+
 else 
-     echo " employee is absent "
+     
+     echo " employee is Absent "
+     SALARY=0
+     echo "Employee Wage Is" : $SALARY
 fi
+ 
